8f97dc8c3faa7634eec7966eb714e1451229182b
ortegaeditor <ortegaeditor@gmail.com>
2026-04-24 15:51:04 +0100

    My first commit

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ fenetre = tk.Tk()
 fenetre.title("Timer")
 fenetre.geometry("800x500")
 
-    # Cadre de gauche
+# Cadre de gauche
 cadre_gauche = tk.Frame(fenetre, bg="lightblue",width=400, height=500)
 cadre_gauche.pack(side="left", fill="both", expand=True)
 
     # Titre
-titre = tk.Label(cadre_gauche,text="Nouvelle",bg="lightblue", font=("Arial", 14,"bold"))
-
-    # Cadre de droite
+titre = tk.Label(cadre_gauche,text="Nouvelle tache",bg="lightblue", font=("Arial", 14,"bold"))
+titre.pack(fill="x", padx=10, pady=10)
+    # Champ de saisie
+        #Nom de la tache
+label_nom = tk.Label(cadre_gauche, text="Nom Tâche", bg="white", font=("Arial", 11))
+label_nom.pack(anchor="w", padx=20, pady=(20, 5))
+        #
+# Cadre de droite
 cadre_droit = tk.Frame(fenetre,bg="lightyellow", width=400, height=500)
 cadre_droit.pack(side="right", fill="both", expand=True)
 
